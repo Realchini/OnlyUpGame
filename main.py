5d2ad863fde07ba990443f4ba12db908ada83fdb
Realchini <realchini@users.noreply.github.com>
2020-06-05 19:02:29 +0300

    Фикс параллакса

--- main.py
+++ main.py
@@ -111,8 +111,8 @@ class Game:
                 Cloud(self)
             self.player.pos.y += max(abs(self.player.vel.y), 2)
             for cloud in self.clouds:
-                prlx = random.randrange(20, 100) / 10
-                cloud.rect.y += max(abs(self.player.vel.y/prlx), 2)
+                prlx = random.randrange(20, 40) / 10
+                cloud.rect.y += max(abs(self.player.vel.y/2), prlx)
             for mob in self.mobs:
                 mob.rect.y += max(abs(self.player.vel.y), 2)
             for plat in self.platforms:
